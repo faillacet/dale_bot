ff1c2b199936717a60c75f51505d1a11c894077d
Angel <amvasquez138@gmail.com>
2021-02-16 19:21:38 -0800

    Added dale responses

--- index.js
+++ index.js
@@ -17,9 +17,12 @@ bot.on('ready', () => {
 bot.on('message', msg => {
     if (msg.content === 'dale' || msg.content === 'Dale'  || msg.content === 'DALE'}) {
         //msg.reply('Srry I no game, I dodge');
-        msg.channel.send("Srry I no game, I dodge");
+        msg.channel.send("Henlo friends");
     }
-
+     
+    if(msg.content === "dale do you want to game?" || msg.content === "dale games?") {
+        msg.channel.send("Srry I have to go make a sandwich");
+    }
     // Play Despacito event
     if (msg.content === 'alexa play despacito') {
         msg.channel.send("!play lana del rey sexy moments 2018");
