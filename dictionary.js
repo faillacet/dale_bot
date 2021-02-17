97c82178f279149cb52a35494edb528501469ccf
Angel <amvasquez138@gmail.com>
2021-02-17 13:30:13 -0800

    Renamed responseArray to randomMessages, added tomorrow.gif

--- dictionary.js
+++ dictionary.js
@@ -5,9 +5,11 @@
 // responseTable.set(1, "anyone want to go Wendy's?");
 // responseTable.set(2, "i h8 survival games, but i'll spend 20 bucks on boost mobile Valheim");
 
-module.exports = responseArray = [
+// Random message array
+module.exports = randomMessages = [
     "League of Legends is the best game of all time",
     "anyone want to go to Wendy's",
     "i h8 survival games, but I'll spend 20 bucks on boost mobile look",
     "my tooth hurts",
+    "Invest in BlackBerry stocks",
 ];
