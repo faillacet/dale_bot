613a04ca92ebbffa0b25496f36f3633890f967b9
Trenton Faillace <faillace@chapman.edu>
2021-04-18 17:56:32 -0700

    I hate dale so he shall now hate me

--- index.js
+++ index.js
@@ -33,9 +33,9 @@ bot.on('ready', () => {
 // msg.channel.send: sends a message to the channnel without tagging anyone
 bot.on('message', msg => {
 
-    if (msg.author.id === "218225932886867968") {                                       // Condition Dale Sends a msg --- 1/1000 chance to delete msg
-        var x = Math.random() * 1000;
-        if (x < 1) {
+    if (msg.author.id === "218225932886867968") {                                       // Condition Dale Sends a msg --- 1/5 chance to delete msg
+        var x = Math.random() * 10;
+        if (x < 2) {
             msg.delete({timeout: 0}).then(msg => console.log('Deleted msg from DALE LOL')).catch(console.error);
         }
     }
