90288ed3946cdd5db48423ae03405d6510ef75ae
Angel Vasquez <anvasquez@csumb.edu>
2022-01-21 14:48:48 -0800

    updated randomeMessages

--- randomDaleMsg.js
+++ randomDaleMsg.js
@@ -1,8 +1,9 @@
 // Random Dale message array
 module.exports = randomMessages = [
-    "League of Legends is the best game of all time",
-    "anyone want to go to Wendy's",
-    "Ketchup is spicy",
-    "At Costco, talk to u later :3",
-    "Can't. Samwich"
+  "League of Legends is the best game of all time",
+  "anyone want to go to Wendy's",
+  "Ketchup is spicy",
+  "At Costco, talk to u later :3",
+  "Can't. Samwich",
+  "I h8 Dota 2",
 ];
