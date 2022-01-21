b9f2d512761624fecacff33a1ad085ec9b7c0071
faillacet <faillace@chapman.edu>
2022-01-20 16:39:43 -0800

    update trant and dale msgs

--- randomDaleMsg.js
+++ randomDaleMsg.js
@@ -2,10 +2,7 @@
 module.exports = randomMessages = [
     "League of Legends is the best game of all time",
     "anyone want to go to Wendy's",
-    "i h8 survival games, but I'll spend 20 bucks on boost mobile games",
-    "anyone want to play eternal return ???",
     "Ketchup is spicy",
-    "Cya I gotta go dodge :)",
     "At Costco, talk to u later :3",
     "Can't. Samwich"
 ];
--- randomTrantMsg.js
+++ randomTrantMsg.js
@@ -1,10 +1,8 @@
 // Random trant message array
 module.exports = randomMessages = [
-    "I like turtles",
     "BIG DOINKS",
-    "lets play rust?",
     "coom",
     "my brother beats me -trant",
     "help, im addicted to POE",
-    "Moe is da wurst",
+    "Moe kinda cute tho...",
 ];
